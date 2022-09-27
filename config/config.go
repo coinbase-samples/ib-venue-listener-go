8b242876032b9d788b32cb64faff7f634ce572ee
Ryan Nitz <ryan.nitz@coinbase.com>
2022-09-27 16:24:41 -0400

    Add aws credential support

--- config/config.go
+++ config/config.go
@@ -1,42 +1,69 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 type AppConfig struct {
-	Env         string `mapstructure:"ENV_NAME"`
-	LogLevel    string `mapstructure:"LOG_LEVEL"`
-	AccessKey   string `mapstructure:"ACCESS_KEY"`
-	SenderId    string `mapstructure:"SENDER_COMPID"`
-	Passphrase  string `mapstructure:"PASSPHRASE"`
-	SigningKey  string `mapstructure:"SIGNING_KEY"`
-	SessionKey  string `mapstructure:"SESSION_KEY"`
-	PrimeApiUrl string `mapstructure:"PRIME_API_URL"`
-	PortfolioId string `mapstructure:"PORTFOLIO_ID"`
+	Env              string `mapstructure:"ENV_NAME"`
+	LogLevel         string `mapstructure:"LOG_LEVEL"`
+	AccessKey        string `mapstructure:"ACCESS_KEY"`
+	SenderId         string `mapstructure:"SENDER_COMPID"`
+	Passphrase       string `mapstructure:"PASSPHRASE"`
+	SigningKey       string `mapstructure:"SIGNING_KEY"`
+	SessionKey       string `mapstructure:"SESSION_KEY"`
+	PrimeApiUrl      string `mapstructure:"PRIME_API_URL"`
+	PortfolioId      string `mapstructure:"PORTFOLIO_ID"`
+	PrimeCredentials string `mapstructure:"PRIME_CREDENTIALS"`
 }
 
-func Setup(app *AppConfig) {
+func (a AppConfig) IsLocalEnv() bool {
+	return a.Env == "local"
+}
+
+func Setup(app *AppConfig) error {
 	viper.AddConfigPath(".")
 	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
 
 	viper.AutomaticEnv()
 	viper.AllowEmptyEnv(true)
-	// set defaults
+
+	// Set defaults
 	viper.SetDefault("LOG_LEVEL", "warning")
 	viper.SetDefault("PORT", "8443")
 	viper.SetDefault("ENV_NAME", "local")
 
 	err := viper.ReadInConfig()
 	if err != nil {
-		fmt.Printf("Missing env file %v\n", err)
+		log.Infof("Missing env file %v\n", err)
 	}
 
 	err = viper.Unmarshal(&app)
 	if err != nil {
-		fmt.Printf("Cannot parse env file %v\n", err)
+		log.Infof("Cannot parse env file %v", err)
 	}
+
+	if app.IsLocalEnv() {
+		return nil
+	}
+
+	// Parse the prime credentials
+	var creds map[string]interface{}
+	err = json.Unmarshal([]byte(app.PrimeCredentials), &creds)
+	if err != nil {
+		return fmt.Errorf("Unable to unmarshal prime credentials: %v", err)
+	}
+
+	app.AccessKey = creds["accessKey"].(string)
+	app.Passphrase = creds["passphrase"].(string)
+	app.SigningKey = creds["signingKey"].(string)
+	app.PortfolioId = creds["portfolioId"].(string)
+	app.SenderId = creds["svcAccountId"].(string)
+
+	return nil
 }
