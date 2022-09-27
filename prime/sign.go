6068f3913affa985e0cacc5d2dcf81f7496b0fa4
Ryan Nitz <ryan.nitz@coinbase.com>
2022-09-27 18:49:42 -0400

    Layout

--- prime/sign.go
+++ prime/sign.go
@@ -8,16 +8,20 @@ import (
 	"strings"
 )
 
-func Sign(channel, key, acctId, ts, portfolioId, productIds, signingKey string) string {
+func Sign(channel, key, acctId, t, portfolioId, productIds, signingKey string) string {
 	h := hmac.New(sha256.New, []byte(signingKey))
 	prodIds := strings.Replace(productIds, "[", "", 1)
 	prodIds = strings.Replace(prodIds, "]", "", 1)
 	prodIds = strings.Replace(prodIds, "\"", "", -1)
 	prodIds = strings.Replace(prodIds, ",", "", -1)
-	//portfolio is blank for prices
-	//string = "#{channelName}#{accessKey}#{svcAcctId}#{timestamp}#{portfolioId}#{productIds}";
-	msg := fmt.Sprintf("%s%s%s%s%s%s", channel, key, acctId, ts, portfolioId, prodIds)
-	fmt.Println("signing", msg)
-	h.Write([]byte(msg))
+	prodIds = strings.Replace(prodIds, " ", "", -1)
+
+	// Portfolio is blank for prices
+	// "#{channelName}#{accessKey}#{svcAcctId}#{timestamp}#{portfolioId}#{productIds}";
+	h.Write([]byte(
+		fmt.Sprintf(
+			"%s%s%s%s%s%s", channel, key, acctId, t, portfolioId, prodIds),
+	),
+	)
 	return base64.StdEncoding.EncodeToString(h.Sum(nil))
 }
